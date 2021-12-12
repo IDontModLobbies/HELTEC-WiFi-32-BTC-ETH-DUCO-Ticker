c089cbf56b809afe80d3e70707c31391b2339b8a
IDontModLobbies <idontmod20@gmail.com>
2021-12-12 13:36:47 -0800

    v3.1 Release
    Added scrolling boot text
    Added Duino-Coin Price and Active Connections
    Added time display on bottom right  corner (24hr clock)
    Added Duino-Coin miner logo

--- BTC/BTC.ino
+++ BTC/BTC.ino
@@ -33,6 +33,15 @@
 #include <U8g2lib.h>
 #include "heltec.h"
 #include "images.h"
+#include <time.h>                              // for time calculations
+#include <WiFiUdp.h>                           // for udp via wifi
+#include <NTPClient.h>
+
+WiFiUDP ntpUDP;
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 //Definitions-----------------------------------------|
 #define FONT_ONE_HEIGHT               8                  // font one height in pixels
@@ -67,6 +76,36 @@ void setup() {
   Heltec.display -> drawXbm(0, 0, IDML_width, IDML_height, IDMLlogo_bits);
   Heltec.display -> display();
   delay(2000);
+
+// Initialize the log buffer
+// allocate memory to store 8 lines of text and 30 chars per line.
+  Heltec.display->setLogBuffer(5, 30);
+
+// Some test data
+  const char* test[] = {
+      "IDontModLobbies",
+      "Bitcoin" ,
+      "Ethereum",
+      "&",
+      "Duino-Coin",
+      "Ticker",
+      "Loading...",
+      "Loading.....",
+      "Loading........",
+      "Loading...........",
+      "Loading.............Complete!"
+  };
+
+  for (uint8_t i = 0; i < 11; i++) {
+    Heltec.display->clear();
+    // Print to the screen
+    Heltec.display->println(test[i]);
+    // Draw it to the internal screen buffer
+    Heltec.display->drawLogBuffer(0, 0);
+    // Display it on the screen
+    Heltec.display->display();
+    delay(500);
+  }
   
 //Draws the WiFI logo
   Heltec.display -> clear();
@@ -105,14 +144,34 @@ void setup() {
   delay(500);
   Heltec.display->clear();
 
-  Serial.print("Connecting to WiFi");
+  Serial.print("Connecting to WiFi...");
   WiFi.begin (ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay (1250);
   }
+  
+// Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
 
+  //United States Time Zones
+  // GMT -10 = -36000 = Hawaii-Aleutian Standard Time
+  // GMT -9 = -32400 = Alaska Standard Time
+  // GMT -8 = -28800 = Pacific Standard Time
+  // GMT -7 = -25200 = Mountain Standard Time
+  // GMT -6 = -21600 = Central Standard Time
+  // GMT -5 = -18000 = Eastern Standard Time
+
+  //Other Examples
+  // GMT 0 = 0
+  // GMT -1 = -3600
+  // GMT +1 = 3600 
+  // GMT +8 = 28800
+  timeClient.setTimeOffset(-28800);
+  timeClient.update();
+  
 // Oled Screen Startup 0.96-inch 128*64 dot matrix OLED.
 
   u8g2.begin();
@@ -125,6 +184,7 @@ void setup() {
   delay(500);
   u8g2.clearBuffer();
 
+  Serial.print("Connected!");
   sprintf(chBuffer, "%s", "WiFi Stats");
   u8g2.drawStr(64 - (u8g2.getStrWidth(chBuffer) / 2), 0, chBuffer);
 
@@ -146,7 +206,6 @@ void setup() {
 
   sprintf(chBuffer, "RSSI: %d", WiFi.RSSI());
   u8g2.drawStr(0, FONT_ONE_HEIGHT, chBuffer);
-
   u8g2.sendBuffer();
   delay (5000);
   u8g2.clearBuffer();
@@ -211,9 +270,14 @@ void loop() {
 
 //Screen Outputs----------------------------|
 
-//Draws the BTC logo.
+//Sets time information
+    String Time = timeClient.getFormattedTime();
+    Serial.println(Time);
+
+//Draws the BTC logo and price in USD.
     String BTC_USD = "$" + (String)bitcoin_usd;
     String BTC_USD24H = "24hr: " + (String)bitcoin_usd_24h_change + "%";
+    timeClient.update();
     Heltec.display -> clear();
     Heltec.display -> drawXbm(0, 0, BTC_width, BTC_height, BTClogo_bits);
     Heltec.display -> display();
@@ -223,6 +287,7 @@ void loop() {
     Heltec.display->drawString(40, 0, "Bitcoin");
     Heltec.display->drawString(40, 20, BTC_USD);
     Heltec.display->drawString(40, 30, BTC_USD24H);
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
@@ -247,9 +312,10 @@ void loop() {
 
     Serial.println("-------------------------------");
     
-//Draws the ETH logo.
+//Draws the ETH logo and price in USD.
     String ETH_USD = "$" + (String)ethereum_usd;
     String ETH_USD24H = "24hr: " + (String)ethereum_usd_24h_change + "%";
+    timeClient.update();
     Heltec.display -> clear();
     Heltec.display -> drawXbm(0, 0, ETH_width, ETH_height, ETHlogo_bits);
     Heltec.display -> display();
@@ -259,12 +325,63 @@ void loop() {
     Heltec.display->drawString(40, 0, "Ethereum (USD)");
     Heltec.display->drawString(40, 20, ETH_USD);
     Heltec.display->drawString(40, 30, ETH_USD24H);
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
   }  
 
-//DUCO
+//////////////////////////////////////////////////////////////////////////////////////
+//DUCO Price
+  if ((WiFi.status() == WL_CONNECTED))
+
+  {
+    HTTPClient http;
+    http.begin ("https://server.duinocoin.com/api.json?ids=%2CActive_connections=true"); //.
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+
+    }
+
+    Serial.println ("\nStatuscode: " + String(httpCode));
+    delay (100);
+
+    const size_t capacity = JSON_OBJECT_SIZE(2) + 2 * JSON_OBJECT_SIZE(6) + 150;
+    DynamicJsonDocument doc(capacity);
+    String payload = http.getString();
+    const char* json = payload.c_str();
+
+    deserializeJson(doc, json);
+    //JsonObject ducostats = doc[filter];
+    float duco_price = doc["Duco price"];
+    int duco_connect = doc["Active connections"];
+    String duco_pricefix = String(duco_price, 4); //4 equals the number of decimals to display.
+
+//Sets time information
+    String Time = timeClient.getFormattedTime();
+    Serial.println(Time);
+    
+//Draws the DUCO logo and Price.
+    String DUCO_PRICE = "Price: " + (String)duco_pricefix;
+    String DUCO_CONNECT = "Active: " + (String)duco_connect;
+    timeClient.update();
+    Heltec.display -> clear();
+    Heltec.display -> drawXbm(0, 0, DUCO_width, DUCO_height, DUCOlogo_bits);
+    Heltec.display -> display();
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(40, 0, "Duino-Coin");
+    Heltec.display->drawString(40, 20, DUCO_PRICE);
+    Heltec.display->drawString(40, 30, DUCO_CONNECT);
+    Heltec.display->drawString(0, 50, Time);
+    Heltec.display -> display();
+    delay(5000);
+    Heltec.display->clear();
+  }
+
+//DUCO Account Balance
   if ((WiFi.status() == WL_CONNECTED))
 
   {
@@ -295,12 +412,18 @@ void loop() {
     Serial.print("DUCO Balance: ");
     Serial.println(duco_bal);
     Serial.println("-------------------------------");
+
+
+//Sets time information
+    String Time = timeClient.getFormattedTime();
+    Serial.println(Time);
     
-//Draws the DUCO logo.
+//Draws the DUCO logo and balance.
     String DUCO_USERNAME = (String)duco_user;
     String DUCO_BALANCE = "Balance: " + (String)duco_bal;
+    timeClient.update();
     Heltec.display -> clear();
-    Heltec.display -> drawXbm(0, 0, DUCO_width, DUCO_height, DUCOlogo_bits);
+    Heltec.display -> drawXbm(0, 0, DUCOMINE_width, DUCOMINE_height, DUCOMINElogo_bits);
     Heltec.display -> display();
 
     Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
@@ -308,6 +431,7 @@ void loop() {
     Heltec.display->drawString(40, 0, "Duino-Coin");
     Heltec.display->drawString(40, 20, DUCO_USERNAME);
     Heltec.display->drawString(40, 30, DUCO_BALANCE);
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
@@ -318,7 +442,7 @@ void loop() {
 
 /*
 
-//EUR BTC
+//Draws the BTC logo and price in EUR.
     String BTC_EUR = "€" + (String)bitcoin_eur;
     String BTC_USD24H = "24hr: " + (String)bitcoin_eur_24h_change + "%";
     Heltec.display -> clear();
@@ -330,11 +454,13 @@ void loop() {
     Heltec.display->drawString(40, 0, "Bitcoin (EUR)");
     Heltec.display->drawString(40, 20, BTC_EUR);
     Heltec.display->drawString(40, 30, BTC_EUR24H);
+    timeClient.update();
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
     
-//EUR ETH
+//Draws the ETH logo and price in EUR.
     String ETH_EUR = "€" + (String)ethereum_eur;
     String ETH_EUR24H = "24hr: " + (String)ethereum_eur_24h_change + "%";
     Heltec.display -> clear();
@@ -346,12 +472,14 @@ void loop() {
     Heltec.display->drawString(40, 0, "Ethereum (EUR)");
     Heltec.display->drawString(40, 20, ETH_EUR);
     Heltec.display->drawString(40, 30, ETH_EUR24H);
+    timeClient.update();
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
 
-//GBP BTC
-    String BTC_GBP = (String)bitcoin_gbp;
+//Draws the BTC logo and price in GBP.
+    String BTC_GBP = "£" = (String)bitcoin_gbp;
     String BTC_GBP24H = "24hr: " + (String)bitcoin_gbp_24h_change + "%";
     Heltec.display -> clear();
     Heltec.display -> drawXbm(0, 0, BTC_width, BTC_height, BTClogo_bits);
@@ -362,12 +490,14 @@ void loop() {
     Heltec.display->drawString(40, 0, "Bitcoin");
     Heltec.display->drawString(40, 20, BTC_GBP);
     Heltec.display->drawString(40, 30, BTC_GBP24H);
+    timeClient.update();
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
     
-//GBP EUR
-    String ETH_GBP = (String)ethereum_gbp;
+//Draws the ETH logo and price in GBP.
+    String ETH_GBP = "£" = (String)ethereum_gbp;
     String ETH_GBP24H = "24hr: " + (String)ethereum_gbp_24h_change + "%";
     Heltec.display -> clear();
     Heltec.display -> drawXbm(0, 0, ETH_width, ETH_height, ETHlogo_bits);
@@ -378,6 +508,8 @@ void loop() {
     Heltec.display->drawString(40, 0, "Ethereum (USD)");
     Heltec.display->drawString(40, 20, ETH_GBP);
     Heltec.display->drawString(40, 30, ETH_GBP24H);
+    timeClient.update();
+    Heltec.display->drawString(0, 50, Time);
     Heltec.display -> display();
     delay(5000);
     Heltec.display->clear();
